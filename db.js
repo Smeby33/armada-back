f50ae1c8bd38dbcf43277f553c1eba5175753c23
Emmanuel (Edoh) SAGOUNAMA-MENSAH <smebedoh33@gmail.com>
2025-06-06 10:33:04 +0100

    Add files via upload

--- db.js
+++ db.js
@@ -4,10 +4,10 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 const db = mysql.createPool({
-    host:"srv1903.hstgr.io"  ,
-    user: "u929681960_smeby",
-    password:"DIKIdiki0832",
-    database: "u929681960_armada",
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
     waitForConnections: true,
     connectionLimit: 50,  // Limite de connexions simultanées
     queueLimit: 0
